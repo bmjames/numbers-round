fbde6e83768a1b617445269af8398b90c5d1fee6
Ben James <ben.james@guardian.co.uk>
2014-11-24 21:19:38 +0000

    Add more normalisation rules

--- Main.hs
+++ Main.hs
@@ -114,10 +114,12 @@ minByAbs _ [] = error "minByAbs on empty list"
 normalise :: Expr -> Expr
 normalise (Lit i)            = Lit i
 normalise (Add l (Add l' r)) = normalise $ Add (Add l l') r
+normalise (Add l (Sub l' r)) = normalise $ Sub (Add l l') r
 normalise (Add l r)          = Add (normalise l) (normalise r)
 normalise (Sub l (Add l' r)) = normalise $ Sub (Sub l l') r
 normalise (Sub l r)          = Sub (normalise l) (normalise r)
 normalise (Mul l (Mul l' r)) = normalise $ Mul (Mul l l') r
+normalise (Mul l (Div l' r)) = normalise $ Div (Mul l l') r
 normalise (Mul l r)          = Mul (normalise l) (normalise r)
 normalise (Div l (Mul l' r)) = normalise $ Div (Div l l') r
 normalise (Div l r)          = Div (normalise l) (normalise r)
